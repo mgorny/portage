mgorny/portage

commit fe5e058211d517e80d7b3a4c0fae7f6c3e755b07

Make FEATURES->USE test mapping non-forced
Use an explicit USE_ORDER entry to control mapping FEATURES=test into
default-enabled USE=test. This means that user can now override
USE=test independently of FEATURES=test and the flag is no longer
forced/masked.

pym/portage/package/ebuild/config.py
@@ -286,6 +286,7 @@ class config(object):
 			self.configlist = [
 				self.configdict['env.d'],
 				self.configdict['repo'],
+				self.configdict['featurestest'],
 				self.configdict['pkginternal'],
 				self.configdict['globals'],
 				self.configdict['defaults'],
@@ -449,12 +450,15 @@ class config(object):
 			# back up our incremental variables:
 			self.configdict={}
 			self._use_expand_dict = {}
-			# configlist will contain: [ env.d, globals, defaults, conf, pkg, backupenv, env ]
+			# configlist will contain: [ env.d, globals, featurestest, defaults, conf, pkg, backupenv, env ]
 			self.configlist.append({})
 			self.configdict["env.d"] = self.configlist[-1]
 
 			self.configlist.append({})
 			self.configdict["repo"] = self.configlist[-1]
+
+			self.configlist.append({})
+			self.configdict["featurestest"] = self.configlist[-1]
 
 			self.configlist.append({})
 			self.configdict["pkginternal"] = self.configlist[-1]
@@ -832,7 +836,7 @@ class config(object):
 			# reasonable defaults; this is important as without USE_ORDER,
 			# USE will always be "" (nothing set)!
 			if "USE_ORDER" not in self:
-				self["USE_ORDER"] = "env:pkg:conf:defaults:pkginternal:repo:env.d"
+				self["USE_ORDER"] = "env:pkg:conf:defaults:pkginternal:featurestest:repo:env.d"
 				self.backup_changes("USE_ORDER")
 
 			if "CBUILD" not in self and "CHOST" in self:
@@ -1253,6 +1257,7 @@ class config(object):
 			del self._penv[:]
 			self.configdict["pkg"].clear()
 			self.configdict["pkginternal"].clear()
+			self.configdict["featurestest"].clear()
 			self.configdict["repo"].clear()
 			self.configdict["defaults"]["USE"] = \
 				" ".join(self.make_defaults_use)
@@ -1413,6 +1418,7 @@ class config(object):
 		cp = cpv_getkey(mycpv)
 		cpv_slot = self.mycpv
 		pkginternaluse = ""
+		use_test = ""
 		iuse = ""
 		pkg_configdict = self.configdict["pkg"]
 		previous_iuse = pkg_configdict.get("IUSE")
@@ -1480,6 +1486,21 @@ class config(object):
 
 		if pkginternaluse != self.configdict["pkginternal"].get("USE", ""):
 			self.configdict["pkginternal"]["USE"] = pkginternaluse
+			has_changed = True
+
+		if explicit_iuse is None:
+			explicit_iuse = frozenset(x.lstrip("+-") for x in iuse.split())
+		if eapi_attrs.iuse_effective:
+			iuse_implicit_match = self._iuse_effective_match
+		else:
+			iuse_implicit_match = self._iuse_implicit_match
+
+		if "test" in explicit_iuse or iuse_implicit_match("test"):
+			if "test" in self.features:
+				use_test = "test"
+
+		if use_test != self.configdict["featurestest"].get("USE", ""):
+			self.configdict["featurestest"]["USE"] = use_test
 			has_changed = True
 
 		repo_env = []
@@ -1640,11 +1661,8 @@ class config(object):
 		# package has different IUSE.
 		use = set(self["USE"].split())
 		unfiltered_use = frozenset(use)
-		if explicit_iuse is None:
-			explicit_iuse = frozenset(x.lstrip("+-") for x in iuse.split())
 
 		if eapi_attrs.iuse_effective:
-			iuse_implicit_match = self._iuse_effective_match
 			portage_iuse = set(self._iuse_effective)
 			portage_iuse.update(explicit_iuse)
 			if built_use is not None:
@@ -1656,7 +1674,6 @@ class config(object):
 			self.configdict["pkg"]["IUSE_EFFECTIVE"] = \
 				" ".join(sorted(portage_iuse))
 		else:
-			iuse_implicit_match = self._iuse_implicit_match
 			portage_iuse = self._get_implicit_iuse()
 			portage_iuse.update(explicit_iuse)
 
@@ -1692,21 +1709,17 @@ class config(object):
 			self.get("EBUILD_FORCE_TEST") == "1"
 
 		if "test" in explicit_iuse or iuse_implicit_match("test"):
-			if "test" not in self.features:
-				use.discard("test")
-			elif restrict_test or \
+			if "test" in self.features:
+				if ebuild_force_test and "test" in self.usemask:
+					self.usemask = \
+						frozenset(x for x in self.usemask if x != "test")
+			if restrict_test or \
 				("test" in self.usemask and not ebuild_force_test):
 				# "test" is in IUSE and USE=test is masked, so execution
 				# of src_test() probably is not reliable. Therefore,
 				# temporarily disable FEATURES=test just for this package.
 				self["FEATURES"] = " ".join(x for x in self.features \
 					if x != "test")
-				use.discard("test")
-			else:
-				use.add("test")
-				if ebuild_force_test and "test" in self.usemask:
-					self.usemask = \
-						frozenset(x for x in self.usemask if x != "test")
 
 		# Allow _* flags from USE_EXPAND wildcards to pass through here.
 		use.difference_update([x for x in use \
@@ -1855,12 +1868,6 @@ class config(object):
 		# build and bootstrap flags used by bootstrap.sh
 		iuse_implicit.add("build")
 		iuse_implicit.add("bootstrap")
-
-		# Controlled by FEATURES=test. Make this implicit, so handling
-		# of FEATURES=test is consistent regardless of explicit IUSE.
-		# Users may use use.mask/package.use.mask to control
-		# FEATURES=test for all ebuilds, regardless of explicit IUSE.
-		iuse_implicit.add("test")
 
 		return iuse_implicit
 

cnf/make.globals
@@ -108,7 +108,7 @@ CONFIG_PROTECT="/etc"
 CONFIG_PROTECT_MASK="/etc/env.d"
 
 # Disable auto-use
-USE_ORDER="env:pkg:conf:defaults:pkginternal:repo:env.d"
+USE_ORDER="env:pkg:conf:defaults:pkginternal:featurestest:repo:env.d"
 
 # Mode bits for ${WORKDIR} (see ebuild.5).
 PORTAGE_WORKDIR_MODE="0700"

man/make.conf.5
@@ -1134,7 +1134,7 @@ This variable contains options that control the build behavior of several
 packages.  More information in \fBebuild\fR(5).  Possible USE values
 can be found in \fI/usr/portage/profiles/use.desc\fR.
 .TP
-\fBUSE_ORDER\fR = \fI"env:pkg:conf:defaults:pkginternal:repo:env.d"\fR
+\fBUSE_ORDER\fR = \fI"env:pkg:conf:defaults:pkginternal:featurestest:repo:env.d"\fR
 Determines the precedence of layers in the incremental stacking of the USE
 variable. Precedence decreases from left to right such that env overrides
 pkg, pkg overrides conf, and so forth.
@@ -1162,6 +1162,9 @@ USE from make.defaults and package.use in the profile
 .TP
 .B pkginternal
 USE from \fBebuild\fR(5) IUSE defaults
+.TP
+.B featurestest
+USE=\fBtest\fR implied by FEATURES=\fBtest\fR.
 .TP
 .B repo
 USE from make.defaults and package.use in the repo's profiles/ top dir

pym/portage/eapi.py
@@ -123,7 +123,7 @@ def _get_eapi_attrs(eapi):
 		exports_EBUILD_PHASE_FUNC = (eapi is None or eapi_exports_EBUILD_PHASE_FUNC(eapi)),
 		exports_PORTDIR = (eapi is not None and eapi_exports_PORTDIR(eapi)),
 		exports_ECLASSDIR = (eapi is not None and eapi_exports_ECLASSDIR(eapi)),
-		feature_flag_test = True,
+		feature_flag_test = False,
 		iuse_defaults = (eapi is None or eapi_has_iuse_defaults(eapi)),
 		iuse_effective = (eapi is not None and eapi_has_iuse_effective(eapi)),
 		path_variables_end_with_trailing_slash = (eapi is not None and

pym/portage/tests/resolver/test_features_test_use.py
@@ -1,68 +1,88 @@
-# Copyright 2012 Gentoo Foundation
+# Copyright 2012-2018 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 
 from portage.tests import TestCase
 from portage.tests.resolver.ResolverPlayground import (ResolverPlayground,
 	ResolverPlaygroundTestCase)
 
-class FeaturesTestUse(TestCase):
 
-	def testFeaturesTestUse(self):
-		ebuilds = {
-			"dev-libs/A-1" : {
-				"IUSE": "test"
-			},
-			"dev-libs/B-1" : {
-				"IUSE": "test foo"
-			},
-		}
+class TestDepend(TestCase):
+	ebuilds = {
+		"dev-libs/A-1" : {
+			"IUSE": "test",
+			"DEPEND": "test? ( dev-libs/B )",
+		},
+		"dev-libs/B-1" : {
+		},
+	}
 
-		installed = {
-			"dev-libs/A-1" : {
-				"USE": "",
-				"IUSE": "test"
-			},
-			"dev-libs/B-1" : {
-				"USE": "foo",
-				"IUSE": "test foo"
-			},
-		}
+	installed = {
+		"dev-libs/A-1" : {
+			"USE": "",
+			"IUSE": "test",
+			"DEPEND": "test? ( dev-libs/B )",
+		},
+	}
 
+	def test_default_use_test(self):
+		"""
+		Test that FEATURES=test enables USE=test by default.
+		"""
 		user_config = {
-			"make.conf" : ("FEATURES=test", "USE=\"-test -foo\"")
+			"make.conf" : ("FEATURES=test", "USE=\"\"")
 		}
-
-		test_cases = (
-
-			# USE=test state should not trigger --newuse rebuilds, as
-			# specified in bug #373209, comment #3.
-			ResolverPlaygroundTestCase(
-				["dev-libs/A"],
-				options = {"--newuse": True, "--selective": True},
-				success = True,
-				mergelist = []),
-
-			# USE=-test -> USE=test, with USE=test forced by FEATURES=test
-			ResolverPlaygroundTestCase(
+		test_case = ResolverPlaygroundTestCase(
 				["dev-libs/A"],
 				options = {},
 				success = True,
-				mergelist = ["dev-libs/A-1"]),
+				mergelist = ["dev-libs/B-1", "dev-libs/A-1"])
 
-			# USE=foo -> USE=-foo, with USE=test forced by FEATURES=test
-			ResolverPlaygroundTestCase(
-				["dev-libs/B"],
-				options = {"--newuse": True, "--selective": True},
-				success = True,
-				mergelist = ["dev-libs/B-1"]),
-		)
-
-		playground = ResolverPlayground(ebuilds=ebuilds,
-			installed=installed, user_config=user_config, debug=False)
+		playground = ResolverPlayground(ebuilds=self.ebuilds,
+			user_config=user_config, debug=False)
 		try:
-			for test_case in test_cases:
-				playground.run_TestCase(test_case)
-				self.assertEqual(test_case.test_success, True, test_case.fail_msg)
+			playground.run_TestCase(test_case)
+			self.assertEqual(test_case.test_success, True, test_case.fail_msg)
 		finally:
 			playground.cleanup()
 
+	def test_no_forced_use_test(self):
+		"""
+		Test that FEATURES=test no longer forces USE=test.
+		"""
+		user_config = {
+			"make.conf" : ("FEATURES=test", "USE=\"-test\"")
+		}
+		test_case = ResolverPlaygroundTestCase(
+				["dev-libs/A"],
+				options = {},
+				success = True,
+				mergelist = ["dev-libs/A-1"])
+
+		playground = ResolverPlayground(ebuilds=self.ebuilds,
+			user_config=user_config, debug=False)
+		try:
+			playground.run_TestCase(test_case)
+			self.assertEqual(test_case.test_success, True, test_case.fail_msg)
+		finally:
+			playground.cleanup()
+
+	def test_newuse(self):
+		"""
+		Test that --newuse now detects USE=test changes.
+		"""
+		user_config = {
+			"make.conf" : ("FEATURES=test", "USE=\"\"")
+		}
+		test_case = ResolverPlaygroundTestCase(
+				["dev-libs/A"],
+				options = {"--newuse": True, "--selective": True},
+				success = True,
+				mergelist = ["dev-libs/B-1", "dev-libs/A-1"])
+
+		playground = ResolverPlayground(ebuilds=self.ebuilds,
+			user_config=user_config, debug=False)
+		try:
+			playground.run_TestCase(test_case)
+			self.assertEqual(test_case.test_success, True, test_case.fail_msg)
+		finally:
+			playground.cleanup()

commit 2d7921b5ad69e0431fe03d91cb3a14c33ec092bf

Fix detecting FEATURES=test for USE=test
Fixes: fe5e05821 (Make FEATURES->USE test mapping non-forced)

pym/portage/package/ebuild/config.py
@@ -1472,21 +1472,6 @@ class config(object):
 			self.configdict["pkginternal"]["USE"] = pkginternaluse
 			has_changed = True
 
-		if explicit_iuse is None:
-			explicit_iuse = frozenset(x.lstrip("+-") for x in iuse.split())
-		if eapi_attrs.iuse_effective:
-			iuse_implicit_match = self._iuse_effective_match
-		else:
-			iuse_implicit_match = self._iuse_implicit_match
-
-		if "test" in explicit_iuse or iuse_implicit_match("test"):
-			if "test" in self.features:
-				use_test = "test"
-
-		if use_test != self.configdict["featurestest"].get("USE", ""):
-			self.configdict["featurestest"]["USE"] = use_test
-			has_changed = True
-
 		repo_env = []
 		if repository and repository != Package.UNKNOWN_REPO:
 			repos = []
@@ -1617,6 +1602,23 @@ class config(object):
 
 		if has_changed:
 			self.reset(keeping_pkg=1)
+
+		if explicit_iuse is None:
+			explicit_iuse = frozenset(x.lstrip("+-") for x in iuse.split())
+		if eapi_attrs.iuse_effective:
+			iuse_implicit_match = self._iuse_effective_match
+		else:
+			iuse_implicit_match = self._iuse_implicit_match
+
+		if "test" in explicit_iuse or iuse_implicit_match("test"):
+			if "test" in self.features:
+				use_test = "test"
+
+		if use_test != self.configdict["featurestest"].get("USE", ""):
+			self.configdict["featurestest"]["USE"] = use_test
+			# TODO: can we avoid that?
+			self.reset(keeping_pkg=1)
+			has_changed = True
 
 		env_configdict = self.configdict['env']
 