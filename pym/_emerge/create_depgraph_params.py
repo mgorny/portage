67c8ff49529a0b478bb8cd97c868e5535cd6ead8
Zac Medico <zmedico@gentoo.org>
2018-05-31 01:27:45 -0700

    emerge: make --nodeps disable --dynamic-deps (bug 656942)
    Since --nodeps disable's the depgraph's	_dynamic_deps_preload code,
    calls to BlockerDB can trigger inappropriate calls to the FakeVartree
    _aux_get_wrapper method, triggering event loop recursion. Therefore,
    make --nodeps disable --dynamic-deps, in order to eliminate the
    inappropriate _aux_get_wrapper calls.
    
    Bug: https://bugs.gentoo.org/656942

--- pym/_emerge/create_depgraph_params.py
+++ pym/_emerge/create_depgraph_params.py
@@ -48,7 +48,7 @@ def create_depgraph_params(myopts, myaction):
 	myparams["ignore_soname_deps"] = myopts.get(
 		"--ignore-soname-deps", "y")
 
-	dynamic_deps = myopts.get("--dynamic-deps", "n") != "n"
+	dynamic_deps = myopts.get("--dynamic-deps", "n") != "n" and "--nodeps" not in myopts
 	if dynamic_deps:
 		myparams["dynamic_deps"] = True
 
