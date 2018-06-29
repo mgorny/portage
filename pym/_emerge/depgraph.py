1ac46e76ee9a911f8a2aa615259b86fa92f2d6ec
Zac Medico <zmedico@gentoo.org>
2018-06-28 21:25:14 -0700

    depgraph._show_unsatisfied_blockers: AttributeError unevaluated_atom (bug 659564)
    Only attempt to access the unevaluated_atom attribute for
    package atoms, since soname atoms do not have this attribute.
    
    Bug: https://bugs.gentoo.org/659564

--- pym/_emerge/depgraph.py
+++ pym/_emerge/depgraph.py
@@ -8392,7 +8392,7 @@ class depgraph(object):
 					else:
 						# Display the specific atom from SetArg or
 						# Package types.
-						if atom != atom.unevaluated_atom:
+						if atom.package and atom != atom.unevaluated_atom:
 							# Show the unevaluated atom, since it can reveal
 							# issues with conditional use-flags missing
 							# from IUSE.
